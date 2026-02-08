fc326bbe09118a63eacf8bf1a78326d637ae1904
Mustafa-GR <mustafagomaa95@gmail.com>
2026-02-08 02:59:59 +0200

    added padding and created a column to add the logo

--- lib/features/auth/presentation/view/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/view/screens/sign_in_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class SignInScreen extends StatelessWidget{
+class SignInScreen extends StatelessWidget {
   const SignInScreen({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-body: Padding(padding: EdgeInsets.all(20)),
-    )
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Column()],
+        ),
+      ),
+    );
   }
 }
