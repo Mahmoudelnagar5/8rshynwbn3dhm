11af32563b08d510e4482d0f387756ec58a13a62
Mustafa-GR <mustafagomaa95@gmail.com>
2026-02-08 02:55:40 +0200

    created StatelessWidget to form the Sign in screen

---
+++ lib/features/auth/presentation/view/screens/sign_in_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SignInScreen extends StatelessWidget{
+  const SignInScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+body: Padding(padding: EdgeInsets.all(20)),
+    )
+  }
+}
